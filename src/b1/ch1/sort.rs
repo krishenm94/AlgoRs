5bb62a4f33c511718182fc225e03ce6359f86f36
krishen <krishenm@gmail.com>
2021-05-19 21:57:19 +0800

    organise by book and chapter

---
+++ src/b1/ch1/sort.rs
@@ -0,0 +1,16 @@
+pub fn insertion(input: &Vec<T>){
+
+}
+
+pub fn selection(input: &Vec<T>){
+
+}
+
+pub fn bubble(input: &Vec<T>){
+
+}
+
+pub fn merge(input: &Vec<T>){
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+mod b1;
+
+fn main(){
+    println!("Hello world!");
+
+    let mut vec: Vec<i32> = vec![89, 3, 5, 34, 8, 1, 13, 21, 55, 2, 1];
+    b1::ch1::sort::insertion(vec);
 }
