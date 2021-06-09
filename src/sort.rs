4e2d6c090c4051f3d495b5e47f6283273bb0d7f4
krishen <krishenm@gmail.com>
2021-06-09 13:32:40 +0800

    test merge sort

--- src/sort.rs
+++ src/sort.rs
@@ -59,23 +59,39 @@ where
     }
 }
 
+use std::fmt::Debug;
+
 pub fn merge<T>(input: &mut Vec<T>)
 where
-    T: Ord + Copy,
+    T: Ord + Copy + Debug,
 {
     pub fn merge_<T>(input1: Vec<T>, input2: Vec<T>) -> Vec<T>
     where
         T: Ord + Copy,
     {
+        if input1.is_empty() {
+            return input2;
+        }
+
+        if input2.is_empty() {
+            return input1;
+        }
+
         let mut i: usize = 0;
         let mut j: usize = 0;
-        let mut output: vec![T, input1.len() + input2.len()];
-        for k in 0..input1.len() + input2.len() - 2 {
-            if input1[i] < input2[j] {
-                output[k] = input1[i];
+        let mut output: Vec<T> = Vec::new();
+        while i + j < input1.len() + input2.len() {
+            if i == input1.len() {
+                output.push(input2[j]);
+                j = j + 1;
+            } else if j == input2.len() {
+                output.push(input1[i]);
+                i = i + 1;
+            } else if input1[i] < input2[j] {
+                output.push(input1[i]);
                 i = i + 1;
             } else {
-                output[k] = input2[j];
+                output.push(input2[j]);
                 j = j + 1;
             }
         }
@@ -87,14 +103,14 @@ where
         return;
     }
 
-    let mut half1 = (&input[0..input.len() / 2 - 1]).to_vec();
-    let mut half2 = (&input[input.len() / 2..input.len() - 1]).to_vec();
-
+    let mut half1 = (&input[0..input.len() / 2]).to_vec();
+    let mut half2 = (&input[input.len() / 2..input.len()]).to_vec();
     merge(&mut half1);
     merge(&mut half2);
     let mut output = merge_(half1, half2);
 
-    input = &mut output;
+    input.clear();
+    input.append(&mut output);
 }
 
 #[cfg(test)]
@@ -125,6 +141,13 @@ mod tests {
     fn test_bubble() {
         let mut input: Vec<i32> = INPUT.clone();
         bubble(&mut input);
+        assert::equal(input, OUTPUT.clone());
+    }
+
+    #[test]
+    fn test_merge() {
+        let mut input: Vec<i32> = INPUT.clone();
+        merge(&mut input);
         assert::equal(input, OUTPUT.clone());
     }
 }
