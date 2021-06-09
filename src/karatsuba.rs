ae8f487d73ae65d3a8ea8f7bcda8fefc0b949ed6
krishen <krishenm@gmail.com>
2021-06-09 15:37:33 +0800

    basic karatsuba mult

--- src/karatsuba.rs
+++ src/karatsuba.rs
@@ -1,22 +1,43 @@
-// TBC
-fn mult(x: i64, y: i64, n: i64) -> i64 {
-    if n == 1 {
+pub fn get_magnitude(x: i64) -> u32 {
+    let mut mag: u32 = 0;
+    let mut val = x;
+    while val != 0 {
+        val = val / 10;
+        mag = mag + 1;
+    }
+
+    return mag;
+}
+
+pub fn mult(x: i64, y: i64) -> i64 {
+    if x == 0 || y == 0 {
+        return 0;
+    }
+    let mag_x = get_magnitude(x);
+    let mag_y = get_magnitude(y);
+    println!("Value & Mag x, y: {}, {}, {}, {}", x, mag_x, y, mag_y);
+    if mag_x < 2 && mag_y < 2 {
         return x * y;
     }
 
-    let a = x / (10 ^ (n / 2));
-    let b = x - a;
-    let c = y / (10 ^ (n / 2));
-    let d = y - c;
+    let a = x / (i64::pow(10, mag_x / 2));
+    let b = x - (a * (i64::pow(10, mag_x / 2)));
+    let c = y / (i64::pow(10, mag_y / 2));
+    let d = y - (c * (i64::pow(10, mag_y / 2)));
+
+    println!("{}", (10 ^ (mag_y / 2)));
+    println!("a, b, c, d: {}, {}, {}, {}", a, b, c, d);
 
     let p = a + b;
     let q = c + d;
-    let ac = mult(a, c, n / 2);
-    let bd = mult(b, d, n / 2);
-    let pq = mult(p, q, n / 2);
+    let ac = mult(a, c);
+    let bd = mult(b, d);
+    let pq = mult(p, q);
     let adbc = pq - ac - bd;
 
-    return 10 ^ n * ac + 10 ^ (n - 2) * adbc + bd;
+    // TBC
+    // shortcut: mag_x is used
+    return (i64::pow(10, mag_x)) * ac + (i64::pow(10, mag_x / 2)) * adbc + bd;
 }
 
 #[cfg(test)]
@@ -24,11 +45,17 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_get_magnitude() {
+        let x = 1234567890;
+        let mag = get_magnitude(x);
+        assert::equal(mag, 10);
+    }
+
+    #[test]
     fn test_mult() {
         let x: i64 = 12;
         let y: i64 = 13;
-        let n = 4;
-        let output = mult(x, y, n);
+        let output = mult(x, y);
         assert::equal(output, 156);
     }
 }
