6b2df592bebd3aac4b4cbc242502d383d0431ff2
krishen <krishenm@gmail.com>
2021-06-10 20:54:54 +0800

    count_inversions_draft

--- src/sort.rs
+++ src/sort.rs
@@ -149,6 +149,54 @@ where
     return second_largest_(half1, half2);
 }
 
+pub fn count_inversions<T>(input: &mut Vec<T>) -> usize
+where
+    T: Ord + Copy + Debug,
+{
+    pub fn merge_count_inv<T>(input1: Vec<T>, input2: Vec<T>) -> (usize, Vec<T>)
+    where
+        T: Ord + Copy,
+    {
+        if input1.is_empty() {
+            return (0, input2);
+        }
+
+        if input2.is_empty() {
+            return (0, input1);
+        }
+
+        let mut i: usize = 0;
+        let mut j: usize = 0;
+        let mut inversions: usize = 0;
+        let mut output: Vec<T> = Vec::new();
+        while i + j < input1.len() + input2.len() {
+            if i < input1.len() && (j == input2.len() || input1[i] < input2[j]) {
+                output.push(input1[i]);
+                i = i + 1;
+            } else if j < input2.len() {
+                output.push(input2[j]);
+                j = j + 1;
+                inversions = inversions + input1.len() - i;
+            }
+        }
+
+        return (inversions, output);
+    }
+
+    if input.len() < 2 {
+        return 0;
+    }
+
+    let mut half1 = (&input[0..input.len() / 2]).to_vec();
+    let mut half2 = (&input[input.len() / 2..input.len()]).to_vec();
+    merge(&mut half1);
+    merge(&mut half2);
+    let (inversions, mut output) = merge_count_inv(half1, half2);
+
+    input.clear();
+    input.append(&mut output);
+    return inversions;
+}
 #[cfg(test)]
 mod tests {
     use super::*;
