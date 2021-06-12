5210cd02760d0b7613f6aa4a54b13a6ee832be23
krishen <krishenm@gmail.com>
2021-06-12 19:51:46 +0800

    add data

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@ mod karatsuba;
 mod sort;
 mod strassen;
 
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader, Error, ErrorKind, Read},
+};
+
+fn read_to_ints(filename: &str) -> Result<Vec<i64>, Error> {
+    let file = File::open(filename).expect("File not found!");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
+
 fn main() {
     println!("Hello world!");
-
-    let mut vec: Vec<i32> = vec![89, 3, 5, 34, 8, 1, 13, 21, 55, 2, 1];
-    sort::insertion(&mut vec);
+    let filename = "./data/integers.txt";
+    let mut vec = read_to_ints(filename).ok().unwrap();
+    let inversions = sort::count_inversions(&mut vec);
+    println!("INVERSIONS: {}", inversions);
 }
