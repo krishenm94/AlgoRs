d5cc73259ba5375f29743f902c454b3c2c470cc2
krishen <krishenm@gmail.com>
2021-06-14 02:58:57 +0800

    element equals index

--- src/search.rs
+++ src/search.rs
@@ -1,4 +1,4 @@
-use std::vec::Vec;
+use std::{option::Option, vec::Vec};
 
 // Find maximum of a unimodal vector;
 // a vector with elements ascending till a peak and then decreasing.
@@ -6,14 +6,26 @@ pub fn unimodal_maximum<T>(input: &Vec<T>, start: usize, end: usize) -> T
 where
     T: Ord + Copy,
 {
-    if end - start == 0 {
-        return input[start];
-    }
-
-    if input[(end + start) / 2 + 1] > input[(end + start) / 2] {
-        unimodal_maximum(input, (start + end) / 2 + 1, end)
+    let index = (end + start) / 2;
+    if end == start {
+        input[start]
+    } else if input[index + 1] > input[index] {
+        unimodal_maximum(input, index + 1, end)
     } else {
-        unimodal_maximum(input, 0, (start + end) / 2)
+        unimodal_maximum(input, start, index)
+    }
+}
+
+pub fn element_equals_index(input: &Vec<i32>, start: usize, end: usize) -> Option<usize> {
+    let index: usize = (end + start) / 2;
+    if input[index] == index as i32 {
+        Some(index)
+    } else if end == start {
+        None
+    } else if input[index] < index as i32 {
+        element_equals_index(input, index + 1, end)
+    } else {
+        element_equals_index(input, start, index)
     }
 }
 
@@ -26,6 +38,13 @@ mod tests {
         let input: Vec<i32> = vec![1, 2, 3, 4, 5, 3, 1, -1];
         let target = 5;
         let output = unimodal_maximum(&input, 0, input.len() - 1);
+        assert::equal(output, target)
+    }
+    #[test]
+    fn test_element_equals_index() {
+        let input = vec![-12, 0, 0, 3, 46, 57, 73, 101];
+        let target: Option<usize> = Some(3);
+        let output = element_equals_index(&input, 0, input.len() - 1);
         assert::equal(output, target)
     }
 }
